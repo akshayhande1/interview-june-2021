385a60cba9f8005385ead4e0f0ea3b9a4dbd7d0b
Akshay Hande <86226594+akshayhande1@users.noreply.github.com>
2021-06-22 13:13:05 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -1,9 +1,21 @@
 package exam;
 
 public class CountCombinations {
-
+  static int count=0;
+    public static void partition(int n,int max, String p)
+    {
+      if(n==0)
+      {
+        System.out.println(p);
+        count++;
+        return;
+      }
+      for(int i=Math.in(max,n);i>=1;i--)
+         partition(n-i,i,p+" "+i)
+    }
   public int getNumberOfWays(final int total, final int k) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+    partition(total,k,"");
     return 0;
   }
 
