3cb412b072c148e6716d2b58a83ad75477bb3c63
Akshay Hande <86226594+akshayhande1@users.noreply.github.com>
2021-06-22 13:02:12 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,30 @@
 package exam;
-
+import java.io.*;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    String s="",k="";
+    int m=n;
+    try
+    {
+      if(m<0)
+      {
+        throw new IOException("No is not correct");
+      }
+     
+    }
+    catch(Exception e)
+    {
+       e.printStackTrace();
+    }
+     while(m>0)
+      {
+         s=Integer.toString(m%2)+s;
+         m=m/2;
+      }
+    k=s.reverse();
+    
+    return k;
   }
 }
